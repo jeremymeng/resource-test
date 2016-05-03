3f151a468aa438c0a2a02aa4ec18974c26d4407a
Jeremy Meng <yumeng@microsoft.com>
2016-05-03 14:59:36 -0700

    Add a test resource solution.

---
+++ src/Console-Core/Program.cs
@@ -0,0 +1,23 @@
+﻿using Console_Desktop;
+
+namespace Console_Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var c in Resource1.String1)
+            {
+                if (c == '\r')
+                    System.Console.Write("\\r");
+                else if (c == '\n')
+                    System.Console.Write("\\n");
+                else if (c == '\t')
+                    System.Console.Write("\\t");
+                else
+                    System.Console.Write(c);
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
